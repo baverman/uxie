44ad95d0c66076f49434793629f094c36fdeaee6
Anton Bobrov <bobrov@vl.ru>
2011-05-23 21:41:31 +1100

    action contexts prototype

--- uxie/actions.py
+++ uxie/actions.py
@@ -33,13 +33,42 @@ class Activator(object):
         result = cb(*args)
         return result is None or result
 
+
 class ContextActivator(Activator):
     def __init__(self, context_resolver):
         Activator.__init__(self)
         self.context_resolver = context_resolver
 
+    def get_ctx_and_name(self, name):
+        ctx, _, name = name.partition('/')
+        return ctx, name
+
+    def bind_accel(self, name, desc, accel, callback, *args):
+        ctx, name = self.get_ctx_and_name(name)
+        self.bind(name, desc, callback, *args)
+        self._map(ctx, name, accel)
+
+    def map(self, name, accel):
+        ctx, name = self.get_ctx_and_name(name)
+        self._map(ctx, name, accel)
+
+    def _map(self, ctx, name, accel):
+        key, modifier = km = gtk.accelerator_parse(accel)
+        if key == 0:
+            import warnings
+            warnings.warn("Can't parse %s" % accel)
+
+        self.accel_group.connect_group(key, modifier, gtk.ACCEL_VISIBLE, self.activate)
+        self.shortcuts.setdefault(km, []).append((ctx, name))
+
     def activate(self, group, window, key, modifier):
-        cb, args = self.get_callback_and_args(key, modifier)
-        ctx = self.context_resolver.get_context(window)
-        result = cb(ctx, *args)
-        return result is None or result
+        key_ctx, path = self.shortcuts[(key, modifier)][0]
+        ctx, ctx_obj = self.context_resolver.get_context(window)
+
+        if ctx == key_ctx or key_ctx == 'any':
+            cb, args = self.actions[path][1:]
+
+            result = cb(ctx_obj, *args)
+            return result is None or result
+        else:
+            return False
