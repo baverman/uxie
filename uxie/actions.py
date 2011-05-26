cbc24a2f9b9965a00c8d5f72761dbfbcec625068
Anton Bobrov <bobrov@vl.ru>
2011-05-26 15:32:44 +1100

    revisited context model for actions

--- uxie/actions.py
+++ uxie/actions.py
@@ -1,3 +1,4 @@
+from bisect import bisect
 import gtk
 
 class Activator(object):
@@ -38,6 +39,7 @@ class ContextActivator(Activator):
     def __init__(self, context_resolver):
         Activator.__init__(self)
         self.context_resolver = context_resolver
+        self.generic_shortcuts = {}
 
     def attach(self, window):
         window.add_accel_group(self.accel_group)
@@ -51,35 +53,38 @@ class ContextActivator(Activator):
 
         return False
 
-    def get_ctx_and_name(self, name):
-        ctx, _, name = name.partition('/')
-        return ctx, name
+    def bind_accel(self, ctx, name, desc, accel, callback, priority=0, *args):
+        self.bind(ctx, name, desc, callback, *args)
+        self.map(ctx, name, accel, priority)
 
-    def bind_accel(self, name, desc, accel, callback, *args):
-        ctx, name = self.get_ctx_and_name(name)
-        self.bind(name, desc, callback, *args)
-        self._map(ctx, name, accel)
+    def _add_shortcut(self, km, ctx, name, priority):
+        shortcuts = self.shortcuts.setdefault(km, [])
+        shortcuts.insert(bisect(shortcuts, priority), (priority, ctx, name))
 
-    def map(self, name, accel):
-        ctx, name = self.get_ctx_and_name(name)
-        self._map(ctx, name, accel)
+    def bind(self, ctx, name, desc, callback, *args):
+        self.actions.setdefault(ctx, {})[name] = (desc, callback, args)
+        if name in self.generic_shortcuts:
+            for km, priority in self.generic_shortcuts[name]:
+                self._add_shortcut(km, ctx, name, -priority)
 
-    def _map(self, ctx, name, accel):
+    def map(self, ctx, name, accel, priority=0):
         key, modifier = km = gtk.accelerator_parse(accel)
         if key == 0:
             import warnings
             warnings.warn("Can't parse %s" % accel)
 
         self.accel_group.connect_group(key, modifier, gtk.ACCEL_VISIBLE, self.activate)
-        self.shortcuts.setdefault(km, []).append((ctx, name))
+        if ctx is None:
+            self.generic_shortcuts.setdefault(name, []).append((km, priority))
+        else:
+            self._add_shortcut(km, ctx, name, -priority)
 
     def activate(self, group, window, key, modifier):
-        key_ctx, path = self.shortcuts[(key, modifier)][0]
-        ctx, ctx_obj = self.context_resolver.get_context(window)
+        _, ctx, name = self.shortcuts[(key, modifier)][0]
+        ctx_obj = self.context_resolver.get_context(window, ctx)
 
-        if ctx == key_ctx or key_ctx == 'any':
-            cb, args = self.actions[path][1:]
-
+        if ctx_obj:
+            cb, args = self.actions[ctx][name][1:]
             result = cb(ctx_obj, *args)
             return result is None or result
         else:
